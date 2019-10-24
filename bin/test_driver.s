801ce0d5abc1464d0e262aad2a0bf795c6cb91fd
Mirvy <michael.yelverton89@gmail.com>
2019-10-23 21:18:58 -0700

    added String_equals to string.a

--- bin/test_driver.s
+++ bin/test_driver.s
@@ -1,6 +1,7 @@
 		      .data
 test_string1: .skip 512
-test_string2: .skip 512
+#test_string2: .skip 512
+ ptr_string2: .word 0
 input_prompt: .asciz "Enter a string: "
  result_good: .asciz "Strings Match"
   result_bad: .asciz "Strings MisMatch"
@@ -14,6 +15,7 @@ _start:
 		mov r2, #512
 		bl getstring
 
+/*  test driver for String_equals
 		ldr r1, =input_prompt
 		bl putstring
 
@@ -24,13 +26,19 @@ _start:
 		ldr r1, =test_string1
 		ldr r2, =test_string2
 		bl String_equals
-
+*/
+		bl String_copy
+		ldr r1, =ptr_string2
+		str r0, [r1]
+		ldr r2, =test_string1
+		bl String_equals
 		cmp r0, #0
 		ldrne r1, =result_good
 		ldreq r1, =result_bad
 		bl putstring
 
-terimnate:
+
+terminate:
 		mov r7, #1
 		svc 0
 		.end
---
+++ source/String_copy.s
@@ -0,0 +1,26 @@
+		.global String_copy
+		.extern malloc
+String_copy:
+	    push {r4-r8,r10,r11,lr}	
+        bl String_length
+		cmp r0, #0
+		beq terminate
+		mov r6, r0
+		add r0, #1
+		mov r4, r1
+		bl malloc
+		mov r5, r0
+
+copy_loop:
+		ldrb r7, [r4], #1
+		str r7, [r5], #1
+		sub r6, #1
+		cmp r6, #0
+		bgt copy_loop
+		mov r7, #0
+		str r7, [r5]
+
+terminate:
+		pop {r4-r8,r10,r11,lr}
+		bx lr
+		.end
