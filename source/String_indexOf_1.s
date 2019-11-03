a40f5e736029abb078dd641d806812614af7638f
naomidonato <36245167+naomidonato@users.noreply.github.com>
2019-11-02 18:59:48 -0700

    Update String_indexOf_1.s

--- source/String_indexOf_1.s
+++ source/String_indexOf_1.s
@@ -4,21 +4,22 @@
 
 String_indexOf_1:
 
-	push	 {r4-r8, lr}		@Save contents of registers R1-R8, LR
-	mov	r2, #0		@Set R2 is intialized to zero and is a pointer
+	push	{r4-r8, lr}	@Save contents of registers R1-R8, LR
+	mov	r5, #0		@Set R2 is intialized to zero and is a pointer
+	mov 	r4, r1
 
 loop:
-	ldrb	r3, [r0]		@Load next character of our string
+	ldrb	r3, [r4]	@Load next character of our string
 	cmp	r3, #0		@First, check if the char is a null
-	moveq r2, #-1		@If null is found, set R2 to -1
+	moveq r5, #-1		@If null is found, set R2 to -1
 	beq	endLoop		@If null found, branch to end
-	cmp	r3, r0		@Compare string with desired char
+	cmp	r3, r2		@Compare string with desired char
 	beq	end1		@If desired char found, branch to end
-	add	r0, #1		@If not found, move to next char in string
-	add	r2, #1		@If not found, increment index by 1, keep moving
+	add	r4, #1		@If not found, move to next char in string
+	add	r5, #1		@If not found, increment index by 1, keep moving
 	b	loop		
 
 endLoop:
-	mov	r0, r2		@Move result into R0 for return
+	mov	r0, r5		@Move result into R0 for return
 	pop	{r4-r8, lr}	@Restore saved register contents
 	bx	LR		@Return to calling program
